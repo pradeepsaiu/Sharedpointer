a02b7c2d078beb90f30dbfdfcb8cb68cd5eed995
Pradeep Sai <puppula1@binghamton.edu>
2016-11-30 18:39:59 -0500

    typerasure without casting

--- temp/SharedPtr.hpp
+++ temp/SharedPtr.hpp
@@ -3,20 +3,14 @@
 #include<stdlib.h>
 #include<iostream>
 #include<assert.h>
+
+
 namespace cs540{
 template <typename T>
 class smart_pointer{	
 		public:
 		T 		*ptr;
 		int 		*ref_count;
-	/*	smart_pointer(T *ptr1,int *ref1){
-			ptr = ptr1;
-			ref_count = ref1;
-		}
-		smart_pointer(){
-			ptr=nullptr;
-			ref_count=nullptr;
-		}*/
 	};	
 template <typename T> 
 class SharedPtr{
@@ -25,31 +19,35 @@ class SharedPtr{
 		smart_pointer<T> * data;
 	public:
 		SharedPtr(){
-			std::cout<<"1. Empty Constructor-------------------------------- \n";
+//			std::cout<<"1. Empty Constructor-------------------------------- \n";
 			data = nullptr;
 		}
-	/*	SharedPtr(smart_pointer<T> *d){
-			data=d;
-		}*/
+
 		SharedPtr(const SharedPtr &p){
 //			std::cout<<"1.b Copy Constructor---------------------------------\n"<<std::endl;
-			data = new smart_pointer<T>();
-			data->ptr = p.data->ptr;
-			(*p.data->ref_count)+=1;
-			data->ref_count = p.data->ref_count;
+//			data = new smart_pointer<T>();
+			if(p.data != nullptr){
+				data = new smart_pointer<T>();
+				data->ptr = p.data->ptr;
+				(*p.data->ref_count)+=1;
+				data->ref_count = p.data->ref_count;
+			}
+			else{
+				data =nullptr;
+			}
 	//		std::cout<<*data->ref_count<<"_____";
 		}
 		template<typename U> explicit SharedPtr(U *ptr_to_obj){	
-			std::cout<<"2. Explicit constructor------------------------------\n";
+//			std::cout<<"2. Explicit constructor------------------------------\n";
 			data 			= new smart_pointer<T>();
 			data->ptr		= ptr_to_obj;
 			data->ref_count 	= new int(1);	
 	//		std::cout<<*(data->ref_count)<<"++++++";
 		}
-	/*	SharedPtr(SharedPtr &&p){
+		SharedPtr(SharedPtr &&p){
 			std::cout<<"3 .Move the managed object---------------------------\n";
 
-		}*/
+		}
 		template <typename U> SharedPtr(const SharedPtr<U> &p){
 //			std::cout<<"4.Copy Assignment------------------------------------\n";
 			if(p.data!=nullptr){
@@ -59,11 +57,28 @@ class SharedPtr{
 			data->ptr = p.data->ptr;
 			data->ref_count = p.data->ref_count;
 		}
-//		template <typename U> SharedPtr(SharedPtr<U> &&p){
-//			std::cout<<"5. Move the managed object---------------------------\n";
-//		}
+		template <typename U> SharedPtr(const SharedPtr<U> &p, int j){
+//			helper for static type cast------------------------------------\n";
+			if(p.data!=nullptr){
+				data = new smart_pointer<T>();
+				(*(p.data->ref_count))++;
+			}
+			if(j==0 ){
+//				std::cout<<"static cast";
+				data->ptr = static_cast<T *>(p.data->ptr);
+			}
+			else{
+//				std::cout<<"dynamic cast";
+			//	data->ptr = dynamic_cast<T * >(p.data->ptr);
+			}
+			data->ref_count = p.data->ref_count;
+		}
+
+		template <typename U> SharedPtr(SharedPtr<U> &&p){
+			std::cout<"************************************";
+		}
 		SharedPtr &operator=(const SharedPtr &p){
-			std::cout<<"6. Operator equal------------copy from same type------------------------\n";
+//			std::cout<<"6. Operator equal------------copy from same type------------------------\n";
 			if(p.data!=nullptr){
 				if(data==nullptr){
 					data		= new smart_pointer<T>();
@@ -73,8 +88,6 @@ class SharedPtr{
 					return * this;
 				}
 				*(data->ref_count) -=1;
-//				std::cout<<*(data->ref_count)<<"+++++++++++++++++++";
-//				assert((*data->ref_count)<0 );
 				if(data->ref_count == 0){
 					delete(data->ptr);
 				}
@@ -83,20 +96,43 @@ class SharedPtr{
 				data->ref_count 	= p.data->ref_count;	
 			}
 			else{
-				assert(0&&"undefined in operator = copy form the same");
+				if(data!=nullptr){
+					if((*data->ref_count) == 1){
+						remove(data);
+					}
+					else{(*data->ref_count)-=1;}
+				}
+				data=nullptr;
 			}
 			return *this;
 		}
 		template <typename U> SharedPtr& operator=(SharedPtr<U> &&p){
-			std::cout<<"7. operator = template u &&& move instruction-------------------------\n";
-
-
+//			std::cout<<"7. operator = template u && move instruction-------------------------\n";
+			if(data!=nullptr){
+				if(data->ptr!=nullptr){
+					if((*data->ref_count)==1){
+						remove(data);
+					}
+					else{
+						(*data->ref_count)-=1;
+					}	
+				}
+			}
+			data 		= new smart_pointer<T>();
+			if(p.data!=nullptr && p.data->ptr!=nullptr){
+				data->ptr = nullptr;
+			}
+			else{
+				data->ptr = p.data->ptr;
+				data->ref_count = p.data->ref_count;
+			}
+			return *this;
 		}
 		SharedPtr &operator=(const SharedPtr &&p){
 			std::cout<<"8. double && operator =------------------------------\n";
 		}
 		template <typename U> SharedPtr<T> & operator = (const SharedPtr<U> &p){
-			std::cout<<"9. operator = copying from a different type-------------------\n";
+//			std::cout<<"9. operator = copying from a different type-------------------\n";
 			if(p.data!=nullptr){
 				if(data == nullptr){
 					data 		= new smart_pointer<T>();
@@ -109,6 +145,7 @@ class SharedPtr{
 			//	assert(*(data->ref_count)<0 &&"weirdo error in operator =");
 				if((*data->ref_count) == 0){
 					delete(data->ptr);
+					delete(data->ref_count);
 				}
 				data->ptr 		= p.data->ptr;
 				*(p.data->ref_count)+=1;
@@ -124,42 +161,40 @@ class SharedPtr{
 		void reset(){
 			if(data!=nullptr){
 				if((*data->ref_count)==1){
-					delete(data->ptr);
 					delete(data->ref_count);
-					delete(data);			
-					data=nullptr;
+					delete(data->ptr);
+					delete(data);
+					data=nullptr;			
 				}
 				else{
 					(*data->ref_count) -=1;
 					delete(data);
 					data = nullptr;
-					
 				}
 			}
 		}
 		template <typename U> void reset(U *p){
 			if(data!=nullptr){
 				if((*data->ref_count)==1){
-					delete(data->ref_count);
-					delete(data->ptr);
-					delete(data);
+					remove(data);			
+					data=nullptr;
 				}
 				else{
 					(*data->ref_count)-=1;
 					delete(data);
+					data=nullptr;
 				}
 			}
 			data = new smart_pointer<T>();
 			data->ptr = p;
-			data->ref_count = new int(1);
-
-			
+			data->ref_count = new int(1);			
 		}
 		explicit operator bool() const{
-			if(data!=nullptr)
-				if(data->ptr!=nullptr)
-					return true;
-			return false;
+			if(data==nullptr)
+				return false;			
+			if(data->ptr==nullptr)
+				return false;
+			return true;
 		}
 		T *get() const{
 			if(data!=nullptr)
@@ -172,16 +207,27 @@ class SharedPtr{
 		T *operator->()const{
 			return data->ptr;
 		}
+		void remove(smart_pointer<T> *to_remove){
+			delete(to_remove->ptr);
+			delete(to_remove->ref_count);
+			delete(to_remove);
+		//	to_remove->ptr = nullptr;
+		//	to_remove->ref_count = nullptr;
+		//	to_remove = nullptr;
+		}
 		~SharedPtr(){
 //			std::cout<<"10. Destructor--------------------------------------begin\n";	
 			if(data!=nullptr){
+				if(data->ref_count!=nullptr){
 				if(*(data->ref_count)==1){
 				//	std::cout<<*(data->ref_count)<<"========\n";
 					delete(data->ref_count);
+					if(data->ptr!=nullptr)
 					delete(data->ptr);
 				}
 				else{
 					(*data->ref_count-=1);
+				}
 				}
 				delete(data);	
 			}
@@ -218,18 +264,14 @@ template<typename T,typename U>
 bool operator!=(std::nullptr_t,const SharedPtr<T> &p1){
 	return !(p1==nullptr);
 }
+
 template<typename T,typename U>
 SharedPtr<T> static_pointer_cast(const SharedPtr<U> &sp){
-
-/*	SharedPtr<T> ret;
-	ret.data   		= new smart_pointer<T>();
-	ret.data->ptr 		= static_cast<T *>(sp.data->ptr);
-	ret.data->ref_count 	= sp.data->ref_count;
-	return ret;
-*/	
-//	return SharedPtr<T>(smart_pointer<T>(static_cast<T *>(sp.get()),sp.data->ref_count));
+	return SharedPtr<T>(sp,0);//calling the over loaded static type cast constructor
 }
-
-
+template<typename T,typename U>
+SharedPtr<T> dynamic_pointer_cast(const SharedPtr<U> &sp){
+	return SharedPtr<T>(sp,1);//calling the over loaded static type cast constructor
+}
 }//ending namespace
 #endif
